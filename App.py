35051928013942696b2ba51d75dc63a850def64e
Alexander Lowe <al10g16@soton.ac.uk>
2021-03-03 23:17:22 +0000

    Initial commit of application GUI classes

---
+++ App.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import glomCV
+import os
+import json
+
+class mainApplicationBody:
+    pass
+
+class imagePreviewWindow:
+    pass
+
+class parameterSelection:
+    pass
+
+class layerSelection:
+    pass
+
+class saveLoad:
+    pass
+
+def main():
+    pass
+
+if __name__ = "__main__":
+    main()
